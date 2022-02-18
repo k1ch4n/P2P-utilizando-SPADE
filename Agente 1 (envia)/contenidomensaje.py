ef4d5a271afbe60989f211eb02d3d1d6e50ef384
XtiaN <65335377+k1ch4n@users.noreply.github.com>
2022-02-18 16:47:42 -0500

    se actualiza contenido para ejecutar un programa
    El programa q se ejecutara desde el agente que envía como referencia es el seno de un angulo

--- Agente 1 (envia)/contenidomensaje.py
+++ Agente 1 (envia)/contenidomensaje.py
@@ -1,7 +1,26 @@
 def ContenidoSi():
+	resultado = ""
+
+	# Calcularemos el seno de x:
+		
+	PI = 3.1459
+	x = PI/6
+	def factorial(a):
+		if a == 0:
+			return 1
+		else:
+			return a*factorial(a-1)
+	#Inicializamos Seno en 0
+	sen = 0
+	#Calculamos el seno de "x" segun la serie de taylor:
+	for n in range(20):
+		sen += (((-1)**(n))/factorial(2*n+1))*(x**(2*n+1))
 	#Se crea el contenido
-	mensaje = "Ejecutado de manera satisfactoria"
-	return mensaje
+	resultado = "El seno de " + str(x) + " es: " + str(sen)
+
+	return resultado
+
+
 def ContenidoNo():
 	#Se crea el contenido
 	mensaje = "El agente esta usando mas del 40% del CPU"
